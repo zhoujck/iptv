78bd659e812217f3c0250ca4811d66f097d7ee01
zhoujck <114135342+zhoujck@users.noreply.github.com>
2025-08-11 17:29:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -404,10 +404,9 @@ def finalize_output(organized, group_order, channel_order):
                 urls = sorted(organized[ip_type][group][channel], key=lambda x: x[1], reverse=True)
                 selected = [u[0] for u in urls[:10]]
 
-                # if selected:
-                #     txt_lines.append(f"{channel},{'#'.join(selected)}")
-               if url in selected:
-                    txt_lines.append(f"{channel},{url}")
+               if selected:
+                    txt_lines.append(f"{channel},{'#'.join(selected)}")
+                   
                     for url in selected:
                         m3u_lines.append(f'#EXTINF:-1 tvg-name="{channel}"tvg-logo="https://gh.catmak.name/https://raw.githubusercontent.com/fanmingming/live/main/tv/{channel}.png" group-title="{group}",{channel}\n{url}')
 
